a4a59d5a3626b5f0e69d390b188956755bb9caf0
Ehsan <ehsan.ashourzadeh0078@gmail.com>
2024-08-08 19:43:31 +0330

    final commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Hello from "./Hello.js";
 import Timer from './Timer';
 import "./style.css";
 
-function App() {
-  return (
-    <div className='main'>
-      <Hello />
-      <Timer />
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <div className='main'>
+        <Hello />
+        <Timer />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Timer.js
+++ src/Timer.js
@@ -1,27 +1,80 @@
+import React from "react";
+import "./style.css";
 
-import React, { useEffect, useState } from "react";
-import "./style.css"
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      isRunning: false,
+    };
+    this.interval = null;
+  }
 
-export default function App() {
-  const [time, setTime] = useState(300);
+  startTimer = () => {
+    if (!this.state.isRunning) {
+      this.setState({ isRunning: true });
+      this.interval = setInterval(() => {
+        this.setState((prevState) => {
+          let { seconds, minutes, hours } = prevState;
 
-  useEffect(() => {
-    let timer = setInterval(() => {
-      setTime((time) => {
-        if (time === 0) {
-          clearInterval(timer);
-          return 0;
-        } else return time - 1;
-      });
-    }, 1000);
-  }, []);
+          seconds += 1;
 
-  return (
-    <div className="App">
-      <p>
-        Time left: {`${Math.floor(time / 60)}`.padStart(2, 0)}:
-        {`${time % 60}`.padStart(2, 0)}
-      </p>
-    </div>
-  );
+          if (seconds === 60) {
+            seconds = 0;
+            minutes += 1;
+          }
+
+          if (minutes === 60) {
+            minutes = 0;
+            hours += 1;
+          }
+
+          return { seconds, minutes, hours };
+        });
+      }, 1000);
+    }
+  };
+
+  stopTimer = () => {
+    clearInterval(this.interval);
+    this.setState({ isRunning: false });
+  };
+
+  resetTimer = () => {
+    this.stopTimer();
+    this.setState({
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  formatTime = (value) => {
+    return value.toString().padStart(2, '0');
+  }
+
+  render() {
+    const { hours, minutes, seconds } = this.state;
+    return (
+      <>
+        <h2 className="Timer">
+          {`${this.formatTime(hours)}:${this.formatTime(minutes)}:${this.formatTime(seconds)}`}
+        </h2>
+        <div className="button_box">
+          <span className="action_button start_button" onClick={this.startTimer}>start</span>
+          <span className="action_button stop_button" onClick={this.stopTimer}>stop</span>
+          <span className="action_button reset_button" onClick={this.resetTimer}>reset</span>
+        </div>
+      </>
+    );
+  }
 }
+
+export default Timer;
